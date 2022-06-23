cc937fcfbb3e31a92ffcdbcdbf9505e20dc6a03a
Igor <filinigor@yandex.ru>
2022-06-23 22:43:02 +0300

    Почти доделал:)

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import classes from './Message.module.css'
 
-function Message() {
+type MessageTypeProps = {
+    avatar: string
+    name: String,
+    message: String,
+    time: String,
+}
+
+
+function Message(props: MessageTypeProps) {
     return (
-        <div>
-           Hello
+        <div className={classes.messages}>
+            <div className={classes.conteinerAvatar}><img src={props.avatar} className={classes.avatar}/></div>
+            <div className={classes.name}>{props.name}
+                <div className={classes.message}>{props.message}</div>
+            </div>
+
+            <div className={classes.time}>{props.time}</div>
         </div>
     )
 }
