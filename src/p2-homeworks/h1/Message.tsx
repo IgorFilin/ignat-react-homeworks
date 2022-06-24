f80be23582227d8e6db8023844d64e8f047fc3d6
Igor <filinigor@yandex.ru>
2022-06-24 14:50:06 +0300

    completed three test

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -13,12 +13,14 @@ function Message(props: MessageTypeProps) {
     return (
         <div className={classes.messages}>
             <div className={classes.conteinerAvatar}><img src={props.avatar} className={classes.avatar}/></div>
-            <div className={classes.name}>{props.name}
+            <div className={classes.conteinerMessage}>
+                <div className={classes.name}>{props.name}</div>
                 <div className={classes.message}>{props.message}</div>
+                <div className={classes.time}>{props.time}</div>
             </div>
 
-            <div className={classes.time}>{props.time}</div>
         </div>
+
     )
 }
 
